014563d1a2b3479312d314fa14492c3866b56070
Ruth Cassidy <ruth.cassidy@zinkworks.com>
2022-06-15 10:53:10 +0100

    Add inner class exercise

--- src/main/java/com/zinkworks/Main.java
+++ src/main/java/com/zinkworks/Main.java
@@ -20,6 +20,19 @@ public class Main {
         System.out.println("Strength = " + ((Monster) werewolf).getStrength());
         saveObject(werewolf);
 
+        // Inner class exercise
+        Gearbox mcLaren = new Gearbox(6);
+        mcLaren.operateClutch(true);
+        mcLaren.changeGear(1);
+        mcLaren.operateClutch(false);
+        System.out.println(mcLaren.wheelSpeed(1000));
+        mcLaren.changeGear(2);
+        System.out.println(mcLaren.wheelSpeed(3000));
+        mcLaren.operateClutch(true);
+        mcLaren.changeGear(3);
+        mcLaren.operateClutch(false);
+        System.out.println(mcLaren.wheelSpeed(6000));
+
     }
 
     public static ArrayList<String> readValues() {
